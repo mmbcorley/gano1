cfe9a39712847f0d1d9a61a8710b446b2e3e4676
Martin Corley <Martin.Corley@ed.ac.uk>
2022-11-07 14:21:39 +0000

    working?

--- experiment.js
+++ experiment.js
@@ -99,18 +99,18 @@ const practice = [
 ];
 
 
-// const factors = {
-//     phrase: ["SC1","SC2","SC3","SC4","FC1","FC2","FC3","FC4"],
-//     target: ["GK1","GK2","GK3","GK4","GK5","GK6","GK7","GK8",
-//  	     "KG1","KG2","KG3","KG4","KG5","KG6","KG7","KG8"]
-// }
+const factors = {
+    phrase: ["SC1","SC2","SC3","SC4","FC1","FC2","FC3","FC4"],
+    target: ["GK1","GK2","GK3","GK4","GK5","GK6","GK7","GK8",
+ 	     "KG1","KG2","KG3","KG4","KG5","KG6","KG7","KG8"]
+}
 
 // for testing
-const factors = {
-    phrase: ["DC1","FC1",],
-    target: ["GK3","GK7",
-	     "KG4","KG5"]
-}
+// const factors = {
+//     phrase: ["DC1","FC1",],
+//     target: ["GK3","GK7",
+// 	     "KG4","KG5"]
+// }
 
 const fullDesign = jsPsych.randomization.factorial(factors,1);
 
